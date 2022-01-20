0ce3a4e36327ea3cf7c6c4dca26b77d6a3c935e1
loick le sausse <rasmo@gmail.com>
2022-01-21 00:07:48 +0100

    modify table comment & crud comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -22,6 +22,16 @@ class Comment
      */
     private $text;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $owner_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $post_id;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +45,30 @@ class Comment
     public function setText(?string $text): self
     {
         $this->text = $text;
+
+        return $this;
+    }
+
+    public function getOwnerId(): ?int
+    {
+        return $this->owner_id;
+    }
+
+    public function setOwnerId(int $owner_id): self
+    {
+        $this->owner_id = $owner_id;
+
+        return $this;
+    }
+
+    public function getPostId(): ?int
+    {
+        return $this->post_id;
+    }
+
+    public function setPostId(int $post_id): self
+    {
+        $this->post_id = $post_id;
 
         return $this;
     }
