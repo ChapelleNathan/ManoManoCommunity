8e6632ec19d8215ed1d929c62047c83c4b9df0b6
Chapelle Nathan <nathanchapelle76@gmail.com>
2022-01-20 16:19:59 +0100

    erased mistakes

--- migrations/Version20220120114910.php
+++ migrations/Version20220120114910.php
@@ -20,15 +20,12 @@ final class Version20220120114910 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment DROP user_ID, DROP post_ID, CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE text text VARCHAR(5000) DEFAULT NULL');
         $this->addSql('ALTER TABLE product CHANGE name name VARCHAR(255) NOT NULL, CHANGE ref ref VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE product-list (id INT AUTO_INCREMENT NOT NULL COMMENT \'Primary Key\', product_id INT DEFAULT NULL, post_id INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE comment ADD user_ID INT DEFAULT NULL, ADD post_ID INT DEFAULT NULL, CHANGE id id INT AUTO_INCREMENT NOT NULL COMMENT \'Primary Key\', CHANGE text text TEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`');
         $this->addSql('ALTER TABLE product CHANGE name name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, CHANGE ref ref VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`');
     }
 }
